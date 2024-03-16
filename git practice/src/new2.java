77a90c4c32a037235339b884aa0394d2ea4eaac6
niranjan <niranjanreddy.karra@gmail.com>
2024-03-16 12:25:53 +0530

    all files commiting

---
+++ git practice/src/new2.java
@@ -0,0 +1,11 @@
+
+public class new2 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("new file");
+
+	}
+
+}
---
+++ git practice/src/ven1.java
@@ -0,0 +1,11 @@
+
+public class ven1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("new file");
+
+	}
+
+}
